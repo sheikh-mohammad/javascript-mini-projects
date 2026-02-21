b13d0b72f5953b7f7da0fbe06f757348f770a697
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-02-21 13:43:07 +0500

    Implement Add Task Feature

--- todo-app/js/app.js
+++ todo-app/js/app.js
@@ -1,1 +1,11 @@
+var task = document.getElementById("task");
+var taskList = document.getElementById("taskList");
 
+function addTask() {
+    taskList.innerHTML += `
+    <div class="task">
+        <span>${task.value}</span>
+        <i class="fa-solid fa-trash-can" id="delete"></i>
+        <i class="fa-solid fa-pen" id="edit"></i>
+    </div>`;
+}
